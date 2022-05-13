c5fc563002eb8e4f6d6ff763b0bf934df71603c0
ss0c <jualso321@gmail.com>
2022-05-12 21:09:55 -0300

    Version funcional sin errores, preestablecido en la dificultad facil

--- sketch_may09a.ino
+++ sketch_may09a.ino
@@ -4,12 +4,15 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 
 
 
-int a; // Numero random
-int b; // Numero random
-int c; // Resultado de los dos numeros random
-int d;
+int randNumb; // Numero random
+int randNumb2; // Numero random
+int resultNumb; // Resultado de los dos numeros random
+int userNumber; // Numero que introduce el usuario
 
-
+// Variables que se usan para la funcion de Leer y mostrar los datos que entrar por el monitor serial
+const byte numChars  = 32;
+char inChars[numChars] ;
+bool newData = true;
 
 void setup() {
 
@@ -21,57 +24,90 @@ void setup() {
 }
 
 void loop() {
- 
+              // Se randomizan dos numero y se imprimen  en el LCD por medio de una funcion
   randomEasy();
+ 
+  resultNumb = randNumb + randNumb2;
   
-  // Serial.println(a);
-  // Serial.println(b);
-  
-  c = a + b;
-  // Serial.println(c);
-  
-  Serial.print(d); 
-  
+              // Se entra a un ciclo infinito que lo que hace es evaluar 
   while(true){
 
-    if(Serial.available()> 0){
-      d = Serial.read();
-
-      if(d == c){
-        lcd.clear();
-        lcd.print("Correcto");  
-        delay(350);
-        break;
+   userNumber = 0;
+   
+   recvWithEndMark();
+   showData();
+   Serial.println(userNumber);
+    if(userNumber != 0){
+   
+   
+   
+    if(userNumber == resultNumb){
+      lcd.clear();
+      lcd.print("Correcto");  
+      delay(350);
+      break;
       }
-      else{
-        lcd.clear();
-        lcd.print("Incorrecto");
-        delay(430);
-        break;
+      
+    else{
+      lcd.clear();
+      lcd.print("Incorrecto");
+      delay(430);
+      break;
       }
-    }
-  } // Llave del while
-}// LLave del loop
-
+    }// Llave del while
+  } 
+}
 
 
 void randomEasy(){
-  a = random(0,5);
-  b = random(0,5);
+  randNumb = random(1,5);
+  randNumb2 = random(1,5);
 
 
   lcd.clear();
   
   lcd.setCursor(0,0);
-  lcd.print(a);
+  lcd.print(randNumb);
   lcd.setCursor(2,0);
   lcd.print("+");
   lcd.setCursor(4,0);
-  lcd.print(b);
+  lcd.print(randNumb2);
   lcd.setCursor(6,0);
   lcd.print("=");
   lcd.setCursor(0,1);
   lcd.blink();
+}
 
 
+void recvWithEndMark(){
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char rc;
+
+  if(Serial.available() > 0){
+    rc = Serial.read();
+
+    if(rc != endMarker){
+      inChars[ndx] = rc;
+      ndx++;
+      
+      if(ndx >= numChars){
+        ndx = numChars - 1;
+      } 
+    }
+    
+    else{
+      inChars[ndx] = "\0";
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+
+void showData(){
+  if(newData == true){
+    userNumber = atoi(inChars);
+    newData = false;
+  }
 }
